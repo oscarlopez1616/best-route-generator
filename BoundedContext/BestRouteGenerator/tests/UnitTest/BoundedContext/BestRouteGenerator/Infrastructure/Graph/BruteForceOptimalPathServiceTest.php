e4809ca21cc87fa06830e8bf2ae1127028586b6c
Oscar López <oscar.lopez@cumlouder.com>
2020-09-27 23:35:21 +0200

    [WIP]

---
+++ BoundedContext/BestRouteGenerator/tests/UnitTest/BoundedContext/BestRouteGenerator/Infrastructure/Graph/BruteForceOptimalPathServiceTest.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+
+namespace BestRouteGenerator\Tests\UnitTest\BoundedContext\BestRouteGenerator\Infrastructure\Graph;
+
+
+use BestRouteGenerator\Domain\Route;
+use BestRouteGenerator\Infrastructure\Graph\BruteForceOptimalPathService;
+use BestRouteGenerator\Tests\ObjectMother\BoundedContext\BestRouteGenerator\Domain\GraphObjectMother;
+use Common\Domain\Exception\DomainException;
+use Common\Type\Id;
+use PHPUnit\Framework\TestCase;
+
+class BruteForceOptimalPathServiceTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function itShouldReturnRoute(): void
+    {
+        $BruteForceOptimalPathService = new BruteForceOptimalPathService();
+        $route = $BruteForceOptimalPathService->findOptimalPath(
+            GraphObjectMother::adjacencyGraphWithExampleDataAllNodesConnected(),
+            new Id('Barcelona')
+        );
+
+        self::assertEquals(
+            new Route(
+                [
+                    new Id('Barcelona'),
+                    new Id('Madrid'),
+                    new Id('Ourense')
+                ]
+            ),
+            $route
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldThrownDomainException(): void
+    {
+        $this->expectException(DomainException::class);
+        $BruteForceOptimalPathService = new BruteForceOptimalPathService();
+        $BruteForceOptimalPathService->findOptimalPath(
+            GraphObjectMother::adjacencyGraphWithExampleDataAllNodesConnected(),
+            new Id('')
+        );
+    }
+
+}
---
+++ BoundedContext/BestRouteGenerator/tests/UnitTest/BoundedContext/BestRouteGenerator/Infrastructure/HarvesineDistanceServiceTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+
+namespace BestRouteGenerator\Tests\UnitTest\BoundedContext\BestRouteGenerator\Infrastructure;
+
+
+use BestRouteGenerator\Domain\City;
+use BestRouteGenerator\Domain\Coordinate;
+use BestRouteGenerator\Domain\Distance;
+use BestRouteGenerator\Infrastructure\HarvesineDistanceService;
+use Common\Type\Id;
+use PHPUnit\Framework\TestCase;
+
+class HarvesineDistanceServiceTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldReturnDistance(): void
+    {
+        $distanceService = new HarvesineDistanceService();
+        $distance = $distanceService->findDistanceInMetersBetween2GpsPointsService(
+            new City(
+                new Id('Beijing'),
+                new Coordinate(39.93, 116.40)
+            ),
+            new City(
+                new Id('Tokyo'),
+                new Coordinate(35.40, 139.45)
+            )
+        );
+
+        self::assertEquals(Distance::createInMeters(2084050.496008833), $distance);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnDistanceWithZero(): void
+    {
+        $distanceService = new HarvesineDistanceService();
+        $distance = $distanceService->findDistanceInMetersBetween2GpsPointsService(
+            new City(
+                new Id('Beijing'),
+                new Coordinate(39.93, 116.40)
+            ),
+            new City(
+                new Id('Beijing'),
+                new Coordinate(39.93, 116.40)
+            ),
+        );
+
+        self::assertEquals(Distance::createInMeters(0), $distance);
+    }
+
+}
